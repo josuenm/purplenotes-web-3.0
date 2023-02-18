b43211f6cbb5f9338aee5c17ea42d4b1d73e0637
josuenm <direct.josue@gmail.com>
2023-02-17 22:03:18 -0300

    feat: password recovery page created

---
+++ src/pages/password-recovery/[id].tsx
@@ -0,0 +1,166 @@
+import { NormalButton } from "@/components/button";
+import { HeaderWithBackButton } from "@/components/header";
+import { Input } from "@/components/input";
+import { useGlobalTools } from "@/contexts/global-tools-conext";
+import { userApi } from "@/services/axios/user-api";
+import {
+  PasswordInfoData,
+  passwordInfoSchema,
+} from "@/services/react-hook-form/password-validation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  return {
+    props: {},
+  };
+}
+
+export default function PasswordRecovery() {
+  const { errorNotification, successNotification } = useGlobalTools();
+  const router = useRouter();
+  const { id } = router.query;
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (!id || typeof id !== "string") return router.push("/");
+
+  const checkPasswordRecovery = useCallback(async () => {
+    setIsLoading(true);
+    const res = await userApi.getPasswordRecovery(id);
+
+    switch (res.status) {
+      case 200:
+        if (res.data.isUsed) {
+          router.push("/sign-in");
+          errorNotification("Password recovery already used");
+          break;
+        }
+        if (Date.now() > new Date(res.data.expiryDate).getTime()) {
+          router.push("/sign-in");
+          errorNotification("Password recovery expired");
+          break;
+        }
+        break;
+
+      default:
+        router.push("/sign-in");
+        errorNotification("Something wrong, try again");
+        break;
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    checkPasswordRecovery();
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PasswordInfoData>({
+    resolver: yupResolver(passwordInfoSchema),
+  });
+
+  const submit = async (data: PasswordInfoData) => {
+    setIsLoading(true);
+    const res = await userApi.confirmPasswordRecovery(id, {
+      password: data.password,
+    });
+
+    switch (res.status) {
+      case 200:
+        router.push("/sign-in");
+        successNotification("Password updated successfully");
+        break;
+
+      case 409:
+        errorNotification("You are already using this password");
+        break;
+
+      default:
+        errorNotification("Something wrong try again");
+        break;
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Password Recovery - Purple Notes</title>
+      </Head>
+
+      <HeaderWithBackButton />
+
+      <main className="safe-area !max-w-[330px] min-h-screen flex flex-col justify-center">
+        <h2 className="text-xl text-center self-center font-medium rounded-md w-fit">
+          Enter your new password.
+        </h2>
+
+        <form
+          className="flex flex-col gap-8 mt-8"
+          onSubmit={handleSubmit(submit)}
+        >
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password:</label>
+            <Input
+              type="password"
+              placeholder="Type your password..."
+              register={register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Confirm your password:</label>
+            <Input
+              type="password"
+              placeholder="Repeat your password..."
+              register={register("passwordConfirmation")}
+            />
+            {errors.passwordConfirmation && (
+              <p className="text-red-500">
+                {errors.passwordConfirmation.message}
+              </p>
+            )}
+          </div>
+
+          <NormalButton>
+            {isLoading ? (
+              <>
+                <svg
+                  aria-hidden="true"
+                  role="status"
+                  className="inline w-5 h-5 mr-3 text-white animate-spin"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="#E5E7EB"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                Loading...
+              </>
+            ) : (
+              "Reset your password"
+            )}
+          </NormalButton>
+        </form>
+      </main>
+    </>
+  );
+}
